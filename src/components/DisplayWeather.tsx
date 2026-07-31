b454fcc81b1a7c06f21e8e7780be932bb7b8b586
Lutricia-The-Coder <lutriciangomane.Zoe@gmail.com>
2026-07-31 15:12:33 +0200

    add weather icons

--- src/components/DisplayWeather.tsx
+++ src/components/DisplayWeather.tsx
@@ -33,11 +33,47 @@ export const DisplayWeather = () => {
 
 
     const fetchCurrentWeather = async (lat:number , lon:number) => {
-const url = `${api_Endpoint}weather?lat=${lat}&lon=${lon}&appid=${api_key}&units =metric`
+const url = `${api_Endpoint}weather?lat=${lat}&lon=${lon}&appid=${api_key}&units=metric`
 const response = await axios.get(url);
 return response.data;
-    }
+    };
 
+
+    const iconChanger = (weather:string) =>{
+        let iconElement: React.ReactNode;
+        let iconColor: string;
+
+        switch(weather){
+     case "Rain" :
+           iconElement = <BsFillCloudRainFill />
+           iconColor="#272829";
+           break;
+
+      case "Clear" :
+           iconElement = <BsFillSunFill />
+           iconColor="#FFC436";
+           break;
+
+              case "Clouds":
+           iconElement = <BsCloudyFill />
+           iconColor="#102C57";
+           break;
+       
+    case "Mist":
+           iconElement = <BsCloudFog2Fill />
+           iconColor="#279EFF";
+           break;
+
+           default:
+            iconElement = <TiWeatherPartlySunny />
+            iconColor="#7B2869"
+        }
+        return (
+            <span className="icon" style={{color:iconColor}}>
+                {iconElement}
+            </span>
+        )
+    }
     React.useEffect(() =>{
 navigator.geolocation.getCurrentPosition((position) => {
     const {latitude , longitude } = position.coords;
@@ -66,7 +102,7 @@ setWeatherData(currentWeather)
             <h1>{weatherData.name}</h1>
             <span>{weatherData.sys.country}</span>
             <div className="icon">
-                  icon
+                  {iconChanger(weatherData.weather[0].main)}
             </div>
             <h1>{weatherData.main.temp}</h1>
             <h2>{weatherData.weather[0].main}</h2>
@@ -76,7 +112,7 @@ setWeatherData(currentWeather)
             <div className="humidityLevel">
               <WiHumidity className="windIcon" />
              <div className="humidInfo">
-                <h1>perc</h1>
+                <h1>{weatherData.main.humidity}%</h1>
                 <p>Humidity</p>
            </div>
             </div>
@@ -84,8 +120,8 @@ setWeatherData(currentWeather)
             <div className="wind">
                 < FaWind  className ="windIcon" />
                   <div className="humidInfo">
-                <h1>km</h1>
-                <p>windspeed</p>
+                <h1>{weatherData.wind.speed}km/h</h1>
+                <p>Wind speed</p>
             </div>
             </div>
         </div>
